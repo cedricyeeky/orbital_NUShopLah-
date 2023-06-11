9798b915a5c3034f1d59dc51bf2819fbeb3f52b9
cedricyeeky <cyky13@gmail.com>
2023-06-11 21:44:48 +0800

    Added logout button for Seller side

--- screens/sellerScreens/SettingsScreen.js
+++ screens/sellerScreens/SettingsScreen.js
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FormButton from '../../components/FormButton';
+import { AuthContext } from '../../navigation/AuthProvider';
+import { firebase } from '../../firebaseconfig';
 
 const SettingsScreen = () => {
-  return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
-  )
+    const {user, logout} = useContext(AuthContext)
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        firebase.firestore().collection('users')
+        .doc(firebase.auth().currentUser.uid).get()
+        .then((snapshot) => {
+            if (snapshot.exists) {
+                setName(snapshot.data())
+            } else {
+                console.log('User does not exist')
+            }
+        })
+    }, [])
+
+    return (
+        //Check how to render firstName of a customer
+        <View style={styles.container}>
+            <Text style={styles.text}>Settings Screen</Text>
+            <FormButton buttonTitle='Logout' onPress={() => logout()} />
+        </View>
+    );
 }
 
-export default SettingsScreen
+export default SettingsScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#f9fafd',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    text: {
+        fontSize: 20,
+        color: '#333333',
+    }
+});
