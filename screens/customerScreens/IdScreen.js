31a2f941c803d872842406deaf2b82ecd5008298
freddychenyouren2 <108988934+freddychenyouren2@users.noreply.github.com>
2023-07-21 14:32:53 +0800

    Update IdScreen.js for testability
    Refactored code

--- screens/customerScreens/IdScreen.js
+++ screens/customerScreens/IdScreen.js
@@ -2,31 +2,51 @@ import React, { useContext, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
 import { AuthContext } from '../../navigation/AuthProvider';
 import { firebase } from '../../firebaseconfig';
-import * as Brightness from 'expo-brightness';
 import QRCodeWithLogo from '../../components/QRCodeWithLogo';
 import { Card } from 'react-native-paper';
 import { useNavigation, useIsFocused } from '@react-navigation/native';
+
+//Export Functions for testing
+export const generateQRCodeData = (uid, firstName, currentPoint, totalPoint) => {
+  if (uid) {
+    const qrCodeData = {
+      uid: uid,
+      firstName,
+      currentPoint,
+      totalPoint,
+      amountPaid: 0,
+      isVoucher: false,
+    };
+    return JSON.stringify(qrCodeData);
+  } else {
+    console.log("(GenerateQRCodeData) User has logged out! No uid anymore");
+    return null; // Return null or an appropriate value when the user is logged out
+  }
+};
+
+export const startFirestoreListener = (user, setCurrentPoint, setTotalPoint) => {
+  const userCollectionRef = firebase.firestore().collection('users');
+  const userDocRef = userCollectionRef.doc(user.uid);
+  const unsubscribe = userDocRef.onSnapshot((snapshot) => {
+    const userData = snapshot.data();
+    if (userData) {
+      const { currentPoint: updatedCurrentPoint, totalPoint: updatedTotalPoint } = userData;
+      setCurrentPoint(updatedCurrentPoint);
+      setTotalPoint(updatedTotalPoint);
+    }
+  });
+
+  return unsubscribe;
+};
+
 
 const IdScreen = () => {
     const { user } = useContext(AuthContext);
     const [currentPoint, setCurrentPoint] = useState(0);
     const [totalPoint, setTotalPoint] = useState(0);
     const [firstName, setFirstName] = useState('');
-    const [previousBrightness, setPreviousBrightness] = useState(0);
     const logoImage = require('../../assets/NUShopLah!.png');
-    const navigation = useNavigation();
-    const isFocused = useIsFocused();
 
-    // useEffect(() => {
-    //   if (isFocused && Platform.OS === 'ios') {
-    //     Brightness.getBrightnessAsync().then((brightness) => {
-    //       setPreviousBrightness(brightness);
-    //       Brightness.setBrightnessAsync(1);
-    //     });
-    //   } else {
-    //     Brightness.setBrightnessAsync(previousBrightness);
-    //   }
-    // }, [isFocused, previousBrightness]);
 
     useEffect(() => {
       //console.log("IdScreen useEffect fetchUserData running...")
@@ -47,6 +67,7 @@ const IdScreen = () => {
         };
   
         // Create a Firestore listener for the user's document
+        // Exported
         const userCollectionRef = firebase.firestore().collection('users');
         const userDocRef = userCollectionRef.doc(user.uid);
         const unsubscribe = userDocRef.onSnapshot((snapshot) => {
@@ -59,6 +80,7 @@ const IdScreen = () => {
         });
     
         fetchUserData();
+        //startFirestoreListener(user, setCurrentPoint, setTotalPoint);
       } else {
         console.log("User has logged out already! Stop fetching user data (Id Screen 2)");
       }
@@ -67,8 +89,6 @@ const IdScreen = () => {
   
 
     useEffect(() => {
-      //Android devices face infinite repetition of this function because brightness level kept changing and functions kept being called.
-      //console.log("IdScreen useEffect running...")
 
       if (user && user.uid) {
         firebase.firestore().collection('users')
@@ -90,14 +110,10 @@ const IdScreen = () => {
         
     }, [user])
 
-    // useEffect(() => {
-    //   Brightness.setBrightnessAsync(1); // Set brightness to maximum when component mounts
-    // }, []);
+    //generateQRCodeData = (user.uid, firstName, currentPoint, totalPoint);
   
     const generateQRCodeData = () => {
       if (user && user.uid) {
-        //Android devices face infinite repetition of this function because brightness level kept changing and functions kept being called.
-        //console.log("Generate QR Code, User UID:", user.uid);
         const qrCodeData = {
           uid: user.uid,
           firstName,
@@ -108,20 +124,27 @@ const IdScreen = () => {
         };
       return JSON.stringify(qrCodeData);
       } else {
-        console.log("(GenerateQRCodeData) User has logged out! No uid anymore")
+        console.log("(GenerateQRCodeData) User has logged out! No uid anymore");
       }
     };
   
     return (
-      <View style={styles.container}>
-        <Card style={styles.card}>
+      <View 
+        style={styles.container}
+        testID='TEST_ID_CONTAINER'
+      >
+        <Card 
+          style={styles.card}
+          testID='TEST_ID_CARD'
+        >
           <Card.Content>
             <Text style={styles.title}>{firstName}'s ID</Text>
-            {/* <Text style={styles.label}>UID:</Text> */}
-            {/* <Text style={styles.text}>{user.uid}</Text> */}
             <Text style={styles.label}>Current Point Balance:</Text>
             <Text style={styles.text}>{currentPoint}</Text>
-            <QRCodeWithLogo value={generateQRCodeData()} logo={logoImage} />
+            <QRCodeWithLogo 
+              value={generateQRCodeData()} 
+              logo={logoImage}
+              testID='qrcode-component' />
           </Card.Content>
         </Card>
       </View>
